de847fff373caeddb91e5d96f5645c27233352c0
iam-SL-eepy <iam.sl.eepy3418@gmail.com>
2017-08-02 20:40:57 -0400

    more testing

--- es6/build/test.bundle.js
+++ es6/build/test.bundle.js
@@ -1,17 +1,22 @@
 let calculateMonthlyPayment = function (principal, years, rate) {
+    let monthlyRate1 = 0;
     if (rate) {
-        monthlyRate = rate / 100 / 12;
+        monthlyRate1 = rate / 100 / 12;
     }
-    let monthlyPayment = principal * monthlyRate / (1 - Math.pow(1 / (1 + monthlyRate), years * 12));
-    return monthlyPayment;
+    let monthlyPayment = principal * monthlyRate1 / (1 - Math.pow(1 / (1 + monthlyRate1), years * 12));
+    return { principal, monthlyRate1 };
 };
 
 document.getElementById('calcBtn').addEventListener('click', function () {
     let principal = document.getElementById("principal").value;
     let years = document.getElementById("years").value;
     let rate = document.getElementById("rate").value;
-    let monthlyPayment = calculateMonthlyPayment(principal, years, rate);
+    let { monthlyPayment, monthlyRate1 } = calculateMonthlyPayment(principal, years, rate);
     document.getElementById("monthlyPayment").innerHTML = monthlyPayment.toFixed(2);
+    document.getElementById("monthlyRateLbl").innerHTML = (monthlyRate1 * 100).toFixed(2);
+    console.log(monthlyRate1);
+    console.log(monthlyRate);
+    console.log(calcBtn);
 });
 var msg = 'something';
 
--- es6/src/calc.js
+++ es6/src/calc.js
@@ -4,13 +4,17 @@ let calculateMonthlyPayment = function (principal, years, rate) {
         monthlyRate = rate / 100 / 12;
     }
     let monthlyPayment = principal * monthlyRate / (1 - (Math.pow(1 / (1 + monthlyRate), years * 12)));
-    return monthlyPayment;
+    return {principal, years, rate, monthlyPayment, monthlyRate};
 };
 
 document.getElementById('calcBtn').addEventListener('click', function () {
     let principal = document.getElementById("principal").value;
     let years = document.getElementById("years").value;
     let rate = document.getElementById("rate").value;
-    let monthlyPayment = calculateMonthlyPayment(principal, years, rate);
+    let {monthlyRate, monthlyPayment} = calculateMonthlyPayment(principal, years, rate);
     document.getElementById("monthlyPayment").innerHTML = monthlyPayment.toFixed(2);
+    document.getElementById("monthlyRateLbl").innerHTML = (monthlyRate * 100).toFixed(2);
+    console.log(monthlyRate1);
+    console.log(monthlyRate);
+    console.log(calcBtn);
 });
